f60bce19f1d5c081de7e36a23a69efd5c703cf5e
AmirInt <amir.intelli@gmail.com>
2024-08-17 15:57:21 +0330

    feat: run naive_em and compare results with k-means

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def run_kmeans():
     for K in range(1, 5):
         best_mixture = None
         best_post = None
-        best_cost = sys.float_info.max
+        best_cost = np.inf
         
         for seed in range(5):
             mixture, post = common.init(X, K, seed)
@@ -22,11 +22,33 @@ def run_kmeans():
                 best_mixture = mixture
             
         common.plot(X, best_mixture, best_post, f"Best of K={K}, cost={cost}")
-        
+
+
+def run_naive_em():
+    X = np.loadtxt("datasets/toy_data.txt")
+    for K in range(1, 5):
+        best_mixture = None
+        best_post = None
+        best_log_likelihood = -np.inf
+
+        for seed in range(5):
+            mixture, post = common.init(X, K, seed)
+            mixture, post, log_likelihood = naive_em.run(X, mixture, post)
+            common.plot(X, mixture, post, f"K={K}, seed={seed}, log_likelihood={log_likelihood}")
+            if log_likelihood > best_log_likelihood:
+                best_log_likelihood = log_likelihood
+                best_post = post
+                best_mixture = mixture
+            
+        common.plot(X, best_mixture, best_post, f"Best of K={K}, log_likelihood={log_likelihood}")
+
 
 def main():
     if sys.argv[1] == "kmeans":
         run_kmeans()
+        
+    if sys.argv[1] == "naive_em":
+        run_naive_em()
 
 
 if __name__ == "__main__":
