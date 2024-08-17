70e6b1e7b72e334305ed26faa471b3074b679d85
AmirInt <amir.intelli@gmail.com>
2024-08-17 14:30:01 +0330

    feat: implement naive_em

--- src/naive_em.py
+++ src/naive_em.py
@@ -4,7 +4,6 @@ import numpy as np
 from src.common import GaussianMixture
 
 
-
 def estep(X: np.ndarray, mixture: GaussianMixture) -> Tuple[np.ndarray, float]:
     """E-step: Softly assigns each datapoint to a gaussian component
 
@@ -17,8 +16,22 @@ def estep(X: np.ndarray, mixture: GaussianMixture) -> Tuple[np.ndarray, float]:
             for all components for all examples
         float: log-likelihood of the assignment
     """
-    raise NotImplementedError
+    n, d = X.shape
+    K = mixture.p.shape[0]
 
+    i_gen_by_j = np.zeros((n, K))
+    post = np.zeros((n, K))
+    for j in range(K):
+        s = X - mixture.mu[j]
+        i_gen_by_j[:, j] = np.exp(-1 / (2 * mixture.var[j]) * (s * s).sum(axis=1)) / np.sqrt(2 * np.pi * mixture.var[j]) ** d
+    
+    post =  mixture.p * i_gen_by_j
+    post /= post.sum(axis=1).reshape(-1, 1)
+
+    log_likelihood = post * np.log(mixture.p * i_gen_by_j / post)
+ 
+    return post, log_likelihood.sum()
+    
 
 def mstep(X: np.ndarray, post: np.ndarray) -> GaussianMixture:
     """M-step: Updates the gaussian mixture by maximizing the log-likelihood
@@ -32,7 +45,17 @@ def mstep(X: np.ndarray, post: np.ndarray) -> GaussianMixture:
     Returns:
         GaussianMixture: the new gaussian mixture
     """
-    raise NotImplementedError
+    n, K = post.shape
+    d = X.shape[1]
+    n_hat = post.sum(axis=0)
+    p_hat = n_hat / n
+    mu_hat = post.T @ X / n_hat.reshape(-1, 1)
+    var_hat = np.zeros(K)
+    for j in range(K):
+        s = X - mu_hat[j]
+        var_hat[j] = (post[:, j] * (s * s).sum(axis=1)).sum() / (d * n_hat[j])
+    
+    return GaussianMixture(mu=mu_hat, var=var_hat, p=p_hat)
 
 
 def run(X: np.ndarray, mixture: GaussianMixture,
@@ -50,4 +73,17 @@ def run(X: np.ndarray, mixture: GaussianMixture,
             for all components for all examples
         float: log-likelihood of the current assignment
     """
-    raise NotImplementedError
+    old_log_likelihood = None
+    new_log_likelihood = None
+    epsilon = 1e-6
+    while old_log_likelihood is None or \
+          new_log_likelihood is None or \
+          abs(new_log_likelihood - old_log_likelihood) >= epsilon * abs(new_log_likelihood):
+        
+        old_log_likelihood = new_log_likelihood
+
+        post, new_log_likelihood = estep(X, mixture)
+
+        mixture = mstep(X, post)
+    
+    return mixture, post, new_log_likelihood
